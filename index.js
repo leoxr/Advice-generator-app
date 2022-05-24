4a49893c69b8dbee4572688e4000ca2d6f0d9cdf
Leonardo Rojo <leofrojo@gmail.com>
2022-05-24 00:22:44 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const adviceNumber = document.getElementById('adviceNumber');
+const advice = document.getElementById('advice');
+const getAdvice = document.getElementById('getAdvice');
+
+
+getAdvice.addEventListener('click', ()=> {
+   
+    dataApi();
+})
+
+window.onload = () => {
+    // dataApi();
+}
+
+function dataApi()  {
+    fetch('https://api.adviceslip.com/advice').then(response => {
+        return response.json()
+    }).then(adviceData => {
+        const adviceObj = adviceData.slip;
+        adviceNumber.innerHTML = `${adviceObj.id}`;
+        advice.innerHTML = `<p class="advice">${adviceObj.advice}</p>`
+    }).catch(error => {
+        console.log(error);
+    });
+        
+}
+
